80ebb4c473fbd5f8ebf24f5ca67de9070ecbefb7
Andrew Bass <arbass@umass.edu>
2017-01-06 13:25:20 -0500

    Added onoff dep and convert client garage to es6 class

--- client/garage.js
+++ client/garage.js
@@ -1,51 +1,55 @@
+"use strict"
+
 var socketio = require('socket.io-client');
 
+class Garage {
+  constructor(garageId, triggerPin, sensorPin) {
+    this.garageId = garageId
+    this.triggerPin  = triggerPin;
+    this.sensorPin = sensorPin;
+
+    // Create a socket to cloud server
+    var socket = socketio('http://localhost:3000', {query: 'garageId=' + this.garageId});
+
+    var self = this;
+
+    socket.on('connect', function() {
+      self.socket = socket;
+    });
+
+    socket.on('open', function() {
+      self.open();
+    });
+
+    socket.on('close', function() {
+      self.close();
+    });
+
+    socket.on('disconnect', function() {
+      console.log("socket disconnected");
+      self.socket = null;
+    });
+  }
+
+  open() {
+    console.log("Opening garage door...");
+  }
+
+  close() {
+    console.log("Closing garage door...");
+  }
+
+  isClosed() {
+    return false;
+  }
+
+  signalOpened() {
+    this.socket.emit('opened');
+  }
+
+  signalClosed() {
+    this.socket.emit('closed');
+  }
+}
+
 module.exports = Garage;
-
-function Garage(garageId, triggerPin, sensorPin) {
-  this.garageId = garageId
-  this.triggerPin  = triggerPin;
-  this.sensorPin = sensorPin;
-
-  // Create a socket to cloud server
-  var socket = socketio('http://localhost:3000', {query: 'garageId=' + this.garageId});
-
-  var self = this;
-
-  socket.on('connect', function() {
-    self.socket = socket;
-  });
-
-  socket.on('open', function() {
-    self.open();
-  });
-
-  socket.on('close', function() {
-    self.close();
-  });
-
-  socket.on('disconnect', function() {
-    console.log("socket disconnected");
-    self.socket = null;
-  });
-}
-
-Garage.prototype.open = function() {
-  console.log("Opening garage door...");
-}
-
-Garage.prototype.close = function() {
-  console.log("Closing garage door...");
-}
-
-Garage.prototype.isClosed = function() {
-  return false;
-}
-
-Garage.prototype.signalOpened = function() {
-  this.socket.emit('opened');
-}
-
-Garage.prototype.signalClosed = function() {
-  this.socket.emit('closed');
-}
